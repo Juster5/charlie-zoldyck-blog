d06c07d85b9b61f93ba33d77b5119a55c1831822
zhongchaolin <zhongchaolin@yy.com>
2022-12-31 00:27:56 +0800

    feat: 中心区域基础内容

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -7,45 +7,51 @@ import { navs } from './constant';
 
 const Header: NextPage = () => {
   return (
-    <header className="okx-header">
-      {/* logo区域 */}
-      <div className="logo-wrapper">
-        <Image src="/images/logo.png" alt="OKX" width={82} height={36} />
-      </div>
-
-      {/* 导航栏区域 */}
-      <div className="navs">
-        {
-          navs.map(el=>{
-            return (
-              <CollpaseMenu menu={el.children} key={el.title}>
-                <div className="nav-item sm-screen-hidden">{el.title}</div>              
-              </CollpaseMenu>
-            )
-          })
-        }
-      </div>
-
-      {/* 登录区域 */}
-      <div className="login-wrapper">
-        <span className="login sm-screen-hidden">Log in</span>
-        <span className="signup">Sign up</span>
-        <span className="menu bg-screen-hidden"></span>
-      </div>
-
-      {/* 多语言区域 */}
-      <CollpaseMenu position="right" 
-        menu={[{
-          title: '简体中文'
-        }, {
-          title: 'English'
-        }]}
-      >
-        <div className="languages sm-screen-hidden" >
-          <span className='okx-header-footer-language'></span>     
+    <>
+      <header className="okx-header">
+        {/* logo区域 */}
+        <div className="logo-wrapper">
+          <Image src="/images/logo.png" alt="OKX" width={82} height={36} />
         </div>
-      </CollpaseMenu>
-    </header>    
+
+        {/* 导航栏区域 */}
+        <div className="navs">
+          {
+            navs.map(el=>{
+              return (
+                <CollpaseMenu menu={el.children} key={el.title}>
+                  <div className="nav-item sm-screen-hidden">
+                    <span>{el.title}</span>
+                    <span className='title-arrow okx-header-footer-arrow-chevrons-down'></span>
+                  </div>              
+                </CollpaseMenu>
+              )
+            })
+          }
+        </div>
+
+        {/* 登录区域 */}
+        <div className="login-wrapper">
+          <span className="login sm-screen-hidden">Log in</span>
+          <span className="signup">Sign up</span>
+          <span className="menu bg-screen-hidden"></span>
+        </div>
+
+        {/* 多语言区域 */}
+        <CollpaseMenu position="right" 
+          menu={[{
+            title: '简体中文'
+          }, {
+            title: 'English'
+          }]}
+        >
+          <div className="languages sm-screen-hidden" >
+            <span className='okx-header-footer-language'></span>     
+          </div>
+        </CollpaseMenu>        
+      </header>    
+      <div className="fixed-block"></div>
+    </>    
   )
 }
 
--- next.config.js
+++ next.config.js
@@ -13,6 +13,39 @@ const nextConfig = {
         one.issuer.and = [path.resolve(__dirname)]
       })
     })
+
+    // todo: 添加sass全局变量
+    // const regexEqual = (x, y) => {
+    //   return x instanceof RegExp &&
+    //   y instanceof RegExp &&
+    //   x.source === y.source &&
+    //   x.global === y.global &&
+    //   x.ignoreCase === y.ignoreCase &&
+    //   x.multiline === y.multiline; 
+    // }
+
+    
+    // const sassRules = config.module.rules
+    //   .find((rule) => typeof rule.oneOf === "object")
+    //   .oneOf.find(
+    //     (rule) =>
+    //       rule.sideEffects === false &&
+    //       regexEqual(rule.test, /\.module\.(scss|sass)$/)
+    //   );
+
+    // sassRules.use = sassRules.use.map((rule) =>
+    //   rule.loader.includes("sass-loader")
+    //     ? {
+    //       ...rule,
+    //       options: {
+    //         ...rule.options,
+    //         //引入你的全局样式
+    //         additionalData: `@import '../styles/variables.scss';`,
+    //       },
+    //     }
+    //     : rule
+    //   );
+   
     return config
   },
 }
