6f37a065a69d39837376ad82855cb01e6d78c3f7
zhongchaolin <zhongchaolin@yy.com>
2022-12-31 11:06:38 +0800

    feat: 添加webkit调试移动端播放问题

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,6 +43,7 @@ export default function Home() {
           title="Faster, better, stronger than your average crypto exchange"
           autoPlay
           playsInline
+          webkit-playsinline="true"          
           loop
           width="280"
           height="580"
@@ -69,6 +70,7 @@ export default function Home() {
             title="Trade like a proGet the lowest fees, fastest transactions, powerful APIs, and more."
             autoPlay
             playsInline
+            webkit-playsinline="true"                    
             loop
             width="936"
             height="536"
@@ -140,6 +142,7 @@ export default function Home() {
             title="Trade like a proGet the lowest fees, fastest transactions, powerful APIs, and more."
             autoPlay
             playsInline
+            webkit-playsinline="true"          
             loop
             width="936"
             height="536"
