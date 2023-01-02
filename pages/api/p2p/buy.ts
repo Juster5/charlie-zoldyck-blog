6e0ea0eb327581cf8a97595c11ff37c3527b6fab
zhongchaolin <zhongchaolin@yy.com>
2023-01-02 14:35:20 +0800

    feat: 调试json

--- pages/api/p2p/buy.ts
+++ pages/api/p2p/buy.ts
@@ -1,19 +1,20 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 // import axios from 'axios'
-const url =
-  'https://www.okx.com/v3/c2c/tradingOrders/books?t=1672584335185&quoteCurrency=USD&baseCurrency=USDT&side=buy&paymentMethod=all&userType=all&hideOverseasVerificationAds=false&sortType=recommended'
-// import { data } from '../data/p2pBuy.json'
-const { curly } = require('node-libcurl')
+// const url =
+// 'https://www.okx.com/v3/c2c/tradingOrders/books?t=1672584335185&quoteCurrency=USD&baseCurrency=USDT&side=buy&paymentMethod=all&userType=all&hideOverseasVerificationAds=false&sortType=recommended'
+import { data } from '../data/p2pBuy.json'
+// const { curly } = require('node-libcurl')
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { data } = await curly.get(url)
+  // const { data } = await curly.get(url)
   res?.status(200).json({
     code: 0,
-    msg: '调试crul',
-    data: data.data,
+    // msg: '调试crul',
+    // data: data.data,
+    data: data,
     // data: {},
   })
 }
